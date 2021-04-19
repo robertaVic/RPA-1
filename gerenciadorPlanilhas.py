6c74dc900b896fbc4bfb64b485f8a02b79c55289
Roberta Costa <roberta.costa@tpfe.com.br>
2021-04-19 14:03:56 -0300

    recentes alterações

---
+++ gerenciadorPlanilhas.py
@@ -0,0 +1,19 @@
+from openpyxl import load_workbook
+import gerenciadorPastas
+
+arquivo_excel = gerenciadorPastas.recuperar_diretorio_usuario() +"\\tpfe.com.br\\SGP e SGC - RPA\\Resultados\\Planilha de Acompanhamento de Solicitações Financeiras 2021.xlsx"
+wb = load_workbook(arquivo_excel) #carregar o arquivo
+sh1 = wb.worksheets[0] #carregar a primeira planilha
+
+ #retorna a ultima linha da planilha
+
+
+
+def preencher_solicitacao_pagamento_avulso(dados_formulario):
+    ultima_linha = sh1.max_row
+    for coluna in range(len(dados_formulario)):
+        sh1.cell(row=ultima_linha+1, column=coluna+1, value=dados_formulario[coluna])
+    wb.save(arquivo_excel)
+    
+
+
--- pgAvulso.py
+++ pgAvulso.py
@@ -7,10 +7,11 @@ import os
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains 
 from openpyxl import load_workbook
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.common.exceptions import TimeoutException
+from gerenciadorPlanilhas import preencher_solicitacao_pagamento_avulso
 
 
 #retornar a data atual
@@ -20,22 +21,9 @@ data_em_texto = today.strftime("%d.%m.%Y")
 
 #caminho da pasta macro(pasta do dia)
 caminho_da_pasta = gerenciadorPastas.recuperar_diretorio_usuario() + "\\tpfe.com.br\\SGP e SGC - RPA\\" + data_em_texto + "\\" 
-#parte do excel
-arquivo_excel = gerenciadorPastas.recuperar_diretorio_usuario() +"\\tpfe.com.br\\SGP e SGC - RPA\\Resultados\\Planilha de Acompanhamento de Solicitações Financeiras 2021.xlsx"
-wb = load_workbook(arquivo_excel) #carregar o arquivo
-sh1 = wb.worksheets[0] #carregar a primeira planilha
-maximo = sh1.max_row #retorna a ultima linha da planilha
-index_row = [] #lista que receberá as linhas vazias
 
 #laço para encontrar as linhas vazias e armazenar na lista acima
-for i in range(8, maximo):
-    #encontrar linhas vazias dentro do range
-    if sh1.cell(row=i, column=1).value in [None,'None']:
-        #armazenar número da linha
-        index_row.append(i)  
-print(index_row)      
-inicio = index_row[0] #armazena a primeira linha utilizável   
-lista = list(range(inicio,maximo)) #criando a lista que será utilizada para o povoamento em excel.
+
 
 #função para tramitar as solicitações
 def pagamentoAvulso(financeiro):
@@ -113,29 +101,34 @@ def pagamentoAvulso(financeiro):
       
         #tempo para salvar todas pastas
         sleep(1.5)
-        # #para cada solicitação marcar a caixa de selecionar todas
-        # financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[3]/div/div/div/table/thead/tr/th[2]/span/span[1]/input").click()
-        # #desmarcar
-        # financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[3]/div/div/div/table/thead/tr/th[2]/span/span[1]/input").click()
+       
         #clicar na caixa de seleção especifica da linha
         financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[3]/div/div/div/table/tbody/tr[1]/td[2]/span/span[1]/input").click()
         #clicar no lápis de edição
         financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[1]/div[3]/div/button[1]").click()
         #PEGAR TODAS AS INFORMAÇOES PARA ALIMENTAR A PLANILHA
-        cnpj = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[2]/div[1]/div/div/div/input").get_attribute("value")
-        banco = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[3]/div[1]/div/div[1]/div/div/div/input").get_attribute("value")
-        agencia = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[3]/div[1]/div/div[2]/div/div/div/input").get_attribute("value")
-        conta = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[3]/div[2]/div/div[1]/div/div/div/input").get_attribute("value")
-        tipo_conta = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[3]/div[2]/div/div[2]/div/div/div/div/div/div/div[1]/input").get_attribute("value")
-        natureza_da_conta = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[4]/div[2]/div/div[3]/div/div/div/div/div/div/div[1]/input").get_attribute("value")
-        forma_de_pagamento = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[4]/div[1]/div/div[1]/div/div/div/div/div/div/div[1]/input").get_attribute("value")
-        valor = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[4]/div[1]/div/div[2]/div/div/div/input").get_attribute("value")
-        valor_pago = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div/input").get_attribute("value")
-        data_solicitada = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[5]/div[1]/div/div[1]/div/div/div/input").get_attribute("value")
-        data_solicitacao = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[5]/div[1]/div/div[2]/div/div/div/input").get_attribute("value")
-        data_pagamento = financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div[5]/div[2]/div/div[1]/div/div/div/input").get_attribute("value")
-
-
+        #criando lista para armazenar todos os dados do formulario
+        dados_do_formulario = []
+        caminho_em_comum_entre_campos_do_formulario = "/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[2]/div/div"
+        #adicionando os valores à lista
+        dados_do_formulario.append(identificador)
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[2]/div[1]/div/div/div/input").get_attribute("value"))
+        dados_do_formulario.append(razao)
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[3]/div[1]/div/div[1]/div/div/div/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[3]/div[1]/div/div[2]/div/div/div/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[3]/div[2]/div/div[1]/div/div/div/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[3]/div[2]/div/div[2]/div/div/div/div/div/div/div[1]/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[4]/div[2]/div/div[3]/div/div/div/div/div/div/div[1]/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[4]/div[1]/div/div[1]/div/div/div/div/div/div/div[1]/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[4]/div[1]/div/div[2]/div/div/div/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[4]/div[2]/div/div[2]/div/div/div/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[5]/div[1]/div/div[1]/div/div/div/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[5]/div[1]/div/div[2]/div/div/div/input").get_attribute("value"))
+        dados_do_formulario.append(financeiro.find_element_by_xpath(caminho_em_comum_entre_campos_do_formulario + "[5]/div[2]/div/div[1]/div/div/div/input").get_attribute("value"))
+        dados_do_formulario.append("")
+        dados_do_formulario.append("")
+        dados_do_formulario.append(estado)
+        
         #clicar em "notas fiscais"
         financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div/div/div[3]/form/fieldset/div/div/div[1]/div/div[2]/div/button[2]").click()
 
@@ -196,29 +189,7 @@ def pagamentoAvulso(financeiro):
         #     else:
         #         comentario = (f"3- {comentario_nao_possui_nota}")
 
-              
-        #povoamento na planilha excel
-        sh1[f"A{lista[0]}"].value = str(identificador)
-        sh1[f"B{lista[0]}"].value = str(cnpj)
-        sh1[f"C{lista[0]}"].value = str(razao) 
-        sh1[f"D{lista[0]}"].value = str(forma_de_pagamento)
-        sh1[f"E{lista[0]}"].value = str(banco)
-        sh1[f"F{lista[0]}"].value = str(agencia)
-        sh1[f"G{lista[0]}"].value = str(conta)
-        sh1[f"H{lista[0]}"].value = str(tipo_conta)
-        sh1[f"I{lista[0]}"].value = str(natureza_da_conta)
-        sh1[f"J{lista[0]}"].value = str(valor)
-        sh1[f"K{lista[0]}"].value = str(valor_pago)
-        sh1[f"L{lista[0]}"].value = str(data_solicitada)
-        sh1[f"M{lista[0]}"].value = str(data_solicitacao)
-        sh1[f"N{lista[0]}"].value = str(data_pagamento)
-        # sh1[f"O{lista[0]}"].value = str(comentario)
-        sh1[f"Q{lista[0]}"].value = str(estado)
-        
-        lista.pop(0)
-
-
-        print(lista) 
+        preencher_solicitacao_pagamento_avulso(dados_do_formulario)
             
         sleep(1.5)
         print("mover arquivos")
@@ -232,11 +203,7 @@ def pagamentoAvulso(financeiro):
             shutil.move(caminho_da_pasta + arquivo, caminho_da_pasta + nome_da_pasta +"\\" + arquivo)
             print("moveu o arquivo!")
 
-        # financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[3]/div/div/div/table/tbody/tr[1]/td[2]/span/span[1]/input").click()
-        # financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[3]/div/div/div/table/thead/tr/th[2]/span/span[1]/input").click()
-        # financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[3]/div/div/div/table/thead/tr/th[2]/span/span[1]/input").click()
-        # financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[3]/div/div/div/table/tbody/tr[1]/td[2]/span/span[1]/input").click()
-        
+        #tramitação das solicitaçoes
         tramitar = financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[1]/div[3]/div/button[2]")
         
         try:
@@ -251,7 +218,7 @@ def pagamentoAvulso(financeiro):
         financeiro.find_element_by_xpath("/html/body/div[5]/div[3]/div/div[2]/ul/div[3]").click()
         sleep(1.5)
 
-        wb.save(arquivo_excel)
+        
         # financeiro.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/main/section/div/div/div/div[1]/div/div[1]/button[2]").click()    
 
     # #para cada solicitação marcar a caixa de selecionar todas
@@ -276,5 +243,4 @@ def pagamentoAvulso(financeiro):
     #     except:
     #         pass
         
-    # except TimeoutException:
-    #     print("deu ruim")
+   
